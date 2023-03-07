99a2c47b035dc25cf7ab0d00f2dda1ee732d4358
AranVmore <122868408+AranVmore@users.noreply.github.com>
2023-03-07 17:17:36 +0100

    httpclient

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,18 +7,29 @@ import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+import { CartComponent } from './cart/cart.component';
+// para registrar los proveedores de HttpClient de Angular a nivel mundial.
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   imports: [
     BrowserModule,
+    HttpClientModule,
     ReactiveFormsModule,
-    RouterModule.forRoot([{ path: '', component: ProductListComponent }]),
+    RouterModule.forRoot([
+      { path: '', component: ProductListComponent },
+      { path: 'products/:productId', component: ProductDetailsComponent },
+      { path: 'cart', component: CartComponent },
+    ]),
   ],
   declarations: [
     AppComponent,
     TopBarComponent,
     ProductListComponent,
     ProductAlertsComponent,
+    ProductDetailsComponent,
+    CartComponent,
   ],
   bootstrap: [AppComponent],
 })
